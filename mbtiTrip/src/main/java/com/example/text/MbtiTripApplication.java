f0d766ec4b5fb1d787e6163e63bf9abd0f8902cb
WOOJIN0526 <zmfhepsl@gmail.com>
2024-03-04 16:15:23 +0900

    2024 03 04 최우진

---
+++ mbtiTrip/src/main/java/com/example/text/MbtiTripApplication.java
@@ -0,0 +1,13 @@
+package com.example.text;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtiTripApplication.class, args);
+	}
+
+}
